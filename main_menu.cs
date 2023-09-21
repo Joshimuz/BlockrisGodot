c08fcf857ee6c1048e87ed388fc62145a2935430
Joshua Edwards <joshua.ed@hotmail.co.uk>
2023-09-21 15:50:01 +0100

    Initial Commit
    Just full of me setting up stuff and messing about with things to get things working.

---
+++ main_menu.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class main_menu : Node2D
+{
+	[Export] PackedScene mainGameScene;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+		GetNode<Button>("Button").Pressed += OnButoootan;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnButoootan()
+	{
+        GetTree().ChangeSceneToPacked(mainGameScene);
+    }
+}
